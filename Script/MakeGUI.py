8d6f74865d58345294492004cfcab2fcd37369d3
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-08-13 16:57:00 +0900

    Create MakeGUI.py

---
+++ Script/MakeGUI.py
@@ -0,0 +1,99 @@
+from PyQt5.QtWidgets import QApplication, QDialog, QTableWidgetItem, QMessageBox
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
+from PyQt5.QtGui import QColor
+from PyQt5 import uic
+import sys
+import StockData as SD  # 주식 데이터 처리 모듈
+
+Save = []
+ThisStockPage = "KR"  # 기본 시장 설정을 한국으로
+
+class StockDataLoader(QThread):
+    dataLoaded = pyqtSignal(list)
+
+    def run(self):
+        global Save
+        stock_data = SD.fetch_stock_data(ThisStockPage)  # 데이터 가져오기
+        Save = stock_data
+        self.dataLoaded.emit(stock_data)
+
+class MyDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()  # UI 설정 초기화
+        self.watchlist = []  # 관심 종목 리스트 초기화
+        self.dataLoader = StockDataLoader()  # StockDataLoader 인스턴스 생성
+        self.dataLoader.dataLoaded.connect(self.updateTable)  # dataLoaded 시그널을 updateTable 슬롯에 연결
+        self.timer = QTimer(self)  # QTimer 인스턴스 생성
+        self.timer.timeout.connect(self.loadStockData)  # 타이머의 timeout 시그널을 loadStockData 메서드에 연결
+        self.timer.start(10000)  # 10초마다 타이머 실행
+        self.loadStockData()  # UI 초기화 후 데이터 로드
+
+    # UI 세팅
+    def setupUi(self):
+        uic.loadUi('tablewidgetTest.ui', self)  # UI 파일 로드
+        self.tableWidget.setRowCount(0)  # 기존 테이블 행 제거
+        self.pushButton.clicked.connect(self.Select_KorMarket)  # 국내
+        self.btn_clearItem.clicked.connect(self.Select_USMarket)  # 해외
+
+    # Update 주식
+    def updateTable(self, stock_data):
+        if not stock_data:
+            QMessageBox.warning(self, "정보", "현재 주식 데이터가 없습니다.")
+            return
+        DrawStock(self, stock_data)
+
+    def Select_KorMarket(self):
+        global ThisStockPage
+        ThisStockPage = "KR"
+        self.loadStockData()  # 데이터 로드
+
+    def Select_USMarket(self):
+        global ThisStockPage
+        ThisStockPage = "US"
+        self.loadStockData()  # 데이터 로드
+
+    def loadStockData(self):
+        self.dataLoader.start()  # 백그라운드 스레드 시작
+
+def DrawStock(self, Data):
+    self.tableWidget.setRowCount(0)  
+    sorted_data = sorted(Data, key=lambda stock: stock.get('number', 0))
+    
+    for stock in sorted_data:
+        row_position = self.tableWidget.rowCount()  # 현재 테이블의 행 수 가져오기
+        self.tableWidget.insertRow(row_position)  # 새로운 행 추가
+
+        name_item = QTableWidgetItem(stock['종목'])
+        Today_Price_item = QTableWidgetItem(format(int(stock['원(￦)']), ","))
+        RiseAndFalls_Percent_item = QTableWidgetItem(format(int(stock['원(￦)'] - stock['시작가']), ","))
+        Start_price_item = QTableWidgetItem(format(int(stock['시작가']), ","))
+
+        if stock['시작가'] != 0:  # 전날 가격이 0이 아닌 경우
+            change_percent = ((stock['원(￦)'] - stock['시작가']) / stock['시작가']) * 100
+        else:
+            change_percent = 0  # 전날 가격이 0일 경우 변경률은 0으로 설정
+
+        RiseAndFalls_Price_item = QTableWidgetItem(f"{change_percent:.2f}%")  # 소수점 두 자리로 포맷팅
+
+        if stock['등락'].startswith('UP'):
+            RiseAndFalls_Percent_item.setForeground(QColor("#f04452"))
+            RiseAndFalls_Price_item.setForeground(QColor("#f04452"))
+        elif stock['등락'].startswith('DOWN'):
+            RiseAndFalls_Percent_item.setForeground(QColor("#3182f6"))
+            RiseAndFalls_Price_item.setForeground(QColor("#3182f6"))
+        else:
+            RiseAndFalls_Percent_item.setForeground(QColor("#333d4b"))
+            RiseAndFalls_Price_item.setForeground(QColor("#333d4b"))
+
+        self.tableWidget.setItem(row_position, 0, name_item)
+        self.tableWidget.setItem(row_position, 1, Today_Price_item)
+        self.tableWidget.setItem(row_position, 2, RiseAndFalls_Percent_item)
+        self.tableWidget.setItem(row_position, 3, RiseAndFalls_Price_item)
+        self.tableWidget.setItem(row_position, 4, Start_price_item)
+
+# 애플리케이션 실행
+app = QApplication(sys.argv)
+dialog = MyDialog()
+dialog.show()
+sys.exit(app.exec_())
