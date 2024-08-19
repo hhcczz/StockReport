454c17439ed3b8fd4361cee834d0d746ad538bb4
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-08-20 02:40:00 +0900

    Create ImageSet.py

---
+++ Script/ImageSet.py
@@ -0,0 +1,12 @@
+ImageLink = {
+    "엔비디아": "https://thumb.tossinvest.com/image/resized/256x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NAS00208X-E0.png",
+    "테슬라" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NAS006XY7-E0.png",
+    
+    "애플" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NAS000C7F-E0.png",
+    "마이크로소프트" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NAS000Q07-E0.png",
+    "알파벳C" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NASWFJYTJ-R0.png",
+    "알파벳A" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NASWFJYTJ-R0.png",
+    "아마존" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-NAS001MF1-E0.png",
+    "메타" : "https://thumb.tossinvest.com/image/resized/32x0/https%3A%2F%2Fstatic.toss.im%2Fpng-icons%2Fsecurities%2Ficn-sec-fill-MVRS.png",
+
+}
