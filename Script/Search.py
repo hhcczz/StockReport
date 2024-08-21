b9ef44dd9a901ec4ddfcf614141fd3854e8b6350
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-08-21 16:39:21 +0900

    Update Search.py

--- Script/Search.py
+++ Script/Search.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox, QCheckBox, QWidget, QHBoxLayout
-from PyQt5.QtGui import QColor, QFont, QFontDatabase
+from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox, QWidget, QHBoxLayout
+from PyQt5.QtGui import QColor, QFont, QFontDatabase, QIcon, QPixmap
 from PyQt5.QtCore import Qt
 import os
 
@@ -34,6 +34,7 @@ class Search:
             row_position = self.gui_instance.tableWidget.rowCount()
             self.gui_instance.tableWidget.insertRow(row_position)
 
+            # 종목 데이터
             name_item = QTableWidgetItem(stock['종목'])
             name_item.setForeground(QColor("#c3c3c6"))
             name_item.setFont(font)
@@ -93,68 +94,28 @@ class Search:
             RiseAndFalls_Percent_item.setForeground(color)
             RiseAndFalls_Percent_item.setFont(font)
 
-            # 현재 페이지에 따라 체크박스 상태를 가져옴
-            #checkbox_checked = False
-            #if self.gui_instance.ThisStockPage == "KR":
-            #    checkbox_checked = self.gui_instance.KR_CheckBoxBoolean.get(stock['종목'], False)
-            #elif self.gui_instance.ThisStockPage in ["US_Dollar", "US_KRW"]:
-            #    checkbox_checked = self.gui_instance.US_CheckBoxBoolean.get(stock['종목'], False)
-            #elif self.gui_instance.ThisStockPage in ["US_ETF_Dollar", "US_ETF_KRW"]:
-            #    checkbox_checked = self.gui_instance.US_ETF_CheckBoxBoolean.get(stock['종목'], False)
+            # 이미지 추가
+            pixmap = load_image(stock['종목'])
+            icon = QTableWidgetItem()
+            icon.setIcon(QIcon(pixmap))
+            self.gui_instance.tableWidget.setItem(row_position, 0, icon)  # 첫 번째 열에 이미지 설정
 
-            #checkbox = QCheckBox()
-            #checkbox.setChecked(checkbox_checked)
-            #checkbox.stateChanged.connect(self.gui_instance.checkBoxStateChanged)
+            self.gui_instance.tableWidget.setItem(row_position, 1, name_item)  # 종목
+            self.gui_instance.tableWidget.setItem(row_position, 2, Today_Price_item)  # 오늘 가격
+            self.gui_instance.tableWidget.setItem(row_position, 3, RiseAndFalls_Price_item)  # 등락(￦)
+            self.gui_instance.tableWidget.setItem(row_position, 4, RiseAndFalls_Percent_item)  # 등락(%)
+            self.gui_instance.tableWidget.setItem(row_position, 5, Start_price_item)  # 시작가
 
-            #widget = QWidget()
-            #layout = QHBoxLayout(widget)
-            #layout.addWidget(checkbox)
-            #layout.setAlignment(Qt.AlignCenter)
-            #layout.setContentsMargins(0, 0, 0, 0)
-            #widget.setLayout(layout)
-
-            #self.gui_instance.tableWidget.setCellWidget(row_position, 5, widget)
-            
-            self.gui_instance.tableWidget.setItem(row_position, 0, name_item)
-            self.gui_instance.tableWidget.setItem(row_position, 1, Today_Price_item)
-            self.gui_instance.tableWidget.setItem(row_position, 2, RiseAndFalls_Price_item)
-            self.gui_instance.tableWidget.setItem(row_position, 3, RiseAndFalls_Percent_item)
-            self.gui_instance.tableWidget.setItem(row_position, 4, Start_price_item)
-
-    #def viewWatchlist(self):
-    #    if not self.gui_instance.watchlist:
-    #        QMessageBox.information(self.gui_instance, "정보", "관심 종목 목록이 비어 있습니다.")
-    #        return
-#
-    #    self.gui_instance.tableWidget_2.setRowCount(0)  # Assume tableWidget_2 is where you show the watchlist
-    #    for stock in self.gui_instance.watchlist:
-    #        row_position = self.gui_instance.tableWidget_2.rowCount()  # 현재 테이블의 행 수 가져오기
-    #        self.gui_instance.tableWidget_2.insertRow(row_position)  # 새로운 행 추가
-#
-    #        name_item = QTableWidgetItem(stock['종목'])
-    #        Today_Price_item = QTableWidgetItem(format(int(stock['원(￦)']), ","))
-    #        RiseAndFalls_Percent_item = QTableWidgetItem(format(int(stock['원(￦)'] - stock['시작가']), ","))
-    #        Start_price_item = QTableWidgetItem(format(int(stock['시작가']), ","))
-#
-    #        if stock['시작가'] != 0:  # 전날 가격이 0이 아닌 경우
-    #            change_percent = ((stock['원(￦)'] - stock['시작가']) / stock['시작가']) * 100
-    #        else:
-    #            change_percent = 0  # 전날 가격이 0일 경우 변경률은 0으로 설정
-#
-    #        RiseAndFalls_Price_item = QTableWidgetItem(f"{change_percent:.2f}%")  # 소수점 두 자리로 포맷팅
-#
-    #        if stock['등락'].startswith('UP'):
-    #            RiseAndFalls_Percent_item.setForeground(QColor("#f04452"))
-    #            RiseAndFalls_Price_item.setForeground(QColor("#f04452"))
-    #        elif stock['등락'].startswith('DOWN'):
-    #            RiseAndFalls_Percent_item.setForeground(QColor("#3182f6"))
-    #            RiseAndFalls_Price_item.setForeground(QColor("#3182f6"))
-    #        else:
-    #            RiseAndFalls_Percent_item.setForeground(QColor("#333d4b"))
-    #            RiseAndFalls_Price_item.setForeground(QColor("#333d4b"))
-#
-    #        self.gui_instance.tableWidget_2.setItem(row_position, 0, name_item)
-    #        self.gui_instance.tableWidget_2.setItem(row_position, 1, Today_Price_item)
-    #        self.gui_instance.tableWidget_2.setItem(row_position, 2, RiseAndFalls_Percent_item)
-    #        self.gui_instance.tableWidget_2.setItem(row_position, 3, RiseAndFalls_Price_item)
-    #        self.gui_instance.tableWidget_2.setItem(row_position, 4, Start_price_item)
+# Helper function to load the image
+def load_image(stock_name):
+    # 절대 경로를 사용하여 경로를 설정합니다
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(current_dir, 'cache_images', f"{stock_name}.png")
+    pixmap = QPixmap(image_path)
+    
+    if pixmap.isNull():
+        print(f"Warning: Unable to load image for stock: {stock_name}")
+        pixmap = QPixmap(64, 64)
+        pixmap.fill(Qt.gray)
+    
+    return pixmap
